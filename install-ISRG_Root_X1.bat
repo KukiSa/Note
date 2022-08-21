3e7717ff9923f9332fedf3346c41dc28be4a0dbb
Signaliks <dreamcrxailing@yahoo.co.jp>
2022-08-21 15:09:03 +0800

    Update install-ISRG_Root_X1.bat
    1. Adjust the display of commands;
    2. Added error judgment.

--- install-ISRG_Root_X1.bat
+++ install-ISRG_Root_X1.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo Welcome to ISRG Root X1 Root Certificate Installer!
+echo Welcome to ISRG Root X1 Installer!
 
 net.exe session 1>NUL 2>NUL && (
     goto as_admin
@@ -9,21 +9,23 @@ net.exe session 1>NUL 2>NUL && (
 )
 
 :as_admin
-@echo on
 mkdir %TMP%\isrgrootx1\
-certutil.exe -urlcache -split -f http://x1.i.lencr.org/ %TMP%\isrgrootx1\ISRGRootX1.crt
 
-certutil.exe -addstore "Root" %TMP%\isrgrootx1\ISRGRootX1.crt
+certutil.exe -urlcache -split -f http://x1.i.lencr.org/ %TMP%\isrgrootx1\ISRGRootX1.crt || goto error
+certutil.exe -addstore "Root" %TMP%\isrgrootx1\ISRGRootX1.crt || goto error
 
-@echo on
 del /q %TMP%\isrgrootx1\
-
 echo Successful installation!
 
 goto end
 
 :not_admin
 echo "ERROR! Please Run it as Administrator!"
+goto end
+
+:error
+echo "ERROR! Access Denied."
+goto end
 
 :end
 pause
