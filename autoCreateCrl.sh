137978f20e6dce3155faa75a72d12946a2e4453c
Signaliks <iwriticons@googlemail.com>
2024-04-07 23:08:56 +0800

    Update autoCreateCrl.sh

--- autoCreateCrl.sh
+++ autoCreateCrl.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#-------------------------------------------------------
+# Requirement:  `openssl`
+# Note:         `openssl` commond is used.
+#-------------------------------------------------------
+# Function:     Use OpenSSL to generate a CRL from a
+#               configuration file.
+# Platform:     Compatible, all OS and arch.
+#
+# Filename:     autoCreateCrl.sh
+# Revision:     1.0.1
+# Date:         February 22, 2023
+# Author:       Signaliks
+# Email:        i@iks.moe
+#-------------------------------------------------------
 
-# ONLY for OpenSSL
 # `-out` is the output path of the CRL file, and `-config` is the path of the root certificate PKI config file.
 openssl ca -gencrl -out /www/wwwroot/pki.iks.moe/static/crl/YoungdoRootCA.crl -config /root/CA/RootCA/rootca.cnf
 openssl ca -gencrl -out /www/wwwroot/pki.iks.moe/static/crl/YoungdoSecureCodeSigningCAR2.crl -config /root/CA/codesignov/powerca.cnf
